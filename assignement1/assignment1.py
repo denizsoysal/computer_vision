f7a77da05997e18686ab03b07bd5018e8304dd83
Deniz Soysal <deniz-soysal@hotmail.com>
2022-03-21 17:32:24 +0100

    [FIX] make disappear parameters

--- assignement1/assignment1.py
+++ assignement1/assignment1.py
@@ -387,11 +387,10 @@ def makeDisappear(input,lower_threshold = np.array([5, 75, 25]),upper_threshold 
     
 def main(input_video_file: str, output_video_file: str) -> None:
     cap,out = OpenVideoAndWriteOutput(input_video_file,output_video_file)
-    # while loop where the real work happens
     time.sleep(3)
     count = 0
     background = 0
-    for i in range(60):
+    for i in range(20):
         ret,background = cap.read()
     background = np.flip(background,axis=1)
     while cap.isOpened():
@@ -401,19 +400,19 @@ def main(input_video_file: str, output_video_file: str) -> None:
         count+=1
         img = np.flip(img,axis=1)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower_red = np.array([0,120,50])
+        lower_red = np.array([0,180,70])
         upper_red = np.array([10,255,255])
         mask1 = cv2.inRange(hsv,lower_red,upper_red)
         lower_red = np.array([170,120,70])
         upper_red = np.array([180,255,255])
         mask2 = cv2.inRange(hsv,lower_red,upper_red)
         mask1 = mask1+mask2
-        mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
-        mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+        mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((9,9),np.uint8))
+        mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((9,9),np.uint8))
         mask2 = cv2.bitwise_not(mask1)
         res1 = cv2.bitwise_and(img,img,mask=mask2)
         res2 = cv2.bitwise_and(background, background, mask = mask1)
-        finalOutput = cv2.addWeighted(res1,1,res2,1,0)
+        finalOutput = cv2.addWeighted(res1,1,res2,1,0)        
         out.write(finalOutput)
         cv2.imshow("magic",finalOutput)
         cv2.waitKey(1)
@@ -574,7 +573,7 @@ def main(input_video_file: str, output_video_file: str) -> None:
     # # Closes all the frames
     # cv2.destroyAllWindows()
 
-main("videos/avocado.mp4", "output.mp4")
+main("videos/avocado2.mp4", "make_disappear2.mp4")
 
 # if __name__ == '__main__':
 #     parser = argparse.ArgumentParser(description='OpenCV video processing')
